5070b07e736936c266267373d28eeb0498d85399
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-03-21 20:41:55 +0530

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -41,7 +41,7 @@ function Hero() {
             href="/"
             className="font-groot-one text-2xl uppercase text-black bg-[#f9d059] border-2 border-[#000000] py-2 px-6 rounded-full"
           >
-            DexToolS
+            DexTool<span>s</span>
           </a>
           <a
             href="https://t.me/minionsoll"
